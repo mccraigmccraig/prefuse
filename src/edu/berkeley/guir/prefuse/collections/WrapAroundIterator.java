3447cc8ad6d1cb2e0a7b7af2f4d8a1fba53de21a
jheer <jheer>
2004-03-12 01:51:51 +0000

    Updated javadoc

--- src/edu/berkeley/guir/prefuse/collections/WrapAroundIterator.java
+++ src/edu/berkeley/guir/prefuse/collections/WrapAroundIterator.java
@@ -9,10 +9,8 @@ import java.util.NoSuchElementException;
  * list, and then wrapping around back to beginning of the list as
  * necessary.
  * 
- * Apr 28, 2003 - jheer - Created class
- * 
  * @version 1.0
- * @author Jeffrey Heer <a href="mailto:jheer@acm.org">jheer@acm.org</a>
+ * @author <a href="http://jheer.org">Jeffrey Heer</a> prefuse(AT)jheer.org
  */
 public class WrapAroundIterator implements Iterator {
 
