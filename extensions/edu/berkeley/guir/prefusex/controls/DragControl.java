6dd6c9dfa8ccb0e5ec13f7f882a97c0814b4440a
jheer <jheer>
2004-03-13 02:16:44 +0000

    Updated drag control

--- extensions/edu/berkeley/guir/prefusex/controls/DragControl.java
+++ extensions/edu/berkeley/guir/prefusex/controls/DragControl.java
@@ -21,7 +21,7 @@ public class DragControl extends ControlAdapter {
     private GraphItem activeItem;
     private Point2D down = new Point2D.Double();
     private Point2D tmp = new Point2D.Double();
-    private boolean wasFixed;
+    private boolean wasFixed, dragged;
     private boolean repaint = true;
     
     public DragControl() {
@@ -34,27 +34,36 @@ public class DragControl extends ControlAdapter {
     public void itemEntered(GraphItem item, MouseEvent e) {
         Display d = (Display)e.getSource();
         d.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-    } //
-    
-    public void itemExited(GraphItem item, MouseEvent e) {
-        Display d = (Display)e.getSource();
-        d.setCursor(Cursor.getDefaultCursor());
-    } //
-    
-    public void itemPressed(GraphItem item, MouseEvent e) {
-        Display d = (Display)e.getComponent();
-        down = d.getAbsoluteCoordinate(e.getPoint(), down);
         activeItem = item;
         wasFixed = item.isFixed();
         item.setFixed(true);
     } //
     
+    public void itemExited(GraphItem item, MouseEvent e) {
+        if ( activeItem == item ) {
+            activeItem = null;
+            item.setFixed(wasFixed);
+        }
+        Display d = (Display)e.getSource();
+        d.setCursor(Cursor.getDefaultCursor());
+    } //
+    
+    public void itemPressed(GraphItem item, MouseEvent e) {
+        dragged = false;
+        Display d = (Display)e.getComponent();
+        down = d.getAbsoluteCoordinate(e.getPoint(), down);
+    } //
+    
     public void itemReleased(GraphItem item, MouseEvent e) {
-        activeItem = null;
-        item.setFixed(wasFixed);
+        if ( dragged ) {
+            activeItem = null;
+            item.setFixed(wasFixed);
+            dragged = false;
+        }
     } //
     
     public void itemDragged(GraphItem item, MouseEvent e) {
+        dragged = true;
         Display d = (Display)e.getComponent();
         tmp = d.getAbsoluteCoordinate(e.getPoint(), tmp);
         double dx = tmp.getX()-down.getX();
