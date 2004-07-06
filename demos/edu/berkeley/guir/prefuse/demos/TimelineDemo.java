530c92d0467abbb8c5f5c1c3ded93e07d3737643
tha_jackl <tha_jackl>
2004-07-06 23:34:57 +0000

    Added nodes for timeline notches and ways to specify number of notches

--- demos/edu/berkeley/guir/prefuse/demos/TimelineDemo.java
+++ demos/edu/berkeley/guir/prefuse/demos/TimelineDemo.java
@@ -1,8 +1,6 @@
+
 /*
  * Created on Jul 3, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package edu.berkeley.guir.prefuse.demos;
 
@@ -13,7 +11,7 @@ import edu.berkeley.guir.prefuse.ItemRegistry;
 import edu.berkeley.guir.prefuse.action.RepaintAction;
 import edu.berkeley.guir.prefuse.action.filter.GraphFilter;
 import edu.berkeley.guir.prefuse.activity.ActionList;
-import edu.berkeley.guir.prefuse.graph.DefaultTreeNode;
+import edu.berkeley.guir.prefuse.graph.DefaultNode;
 import edu.berkeley.guir.prefuse.graph.Graph;
 import edu.berkeley.guir.prefuse.graph.io.XMLGraphReader;
 import edu.berkeley.guir.prefuse.render.DefaultRendererFactory;
@@ -22,32 +20,72 @@ import edu.berkeley.guir.prefusex.controls.DragControl;
 import edu.berkeley.guir.prefusex.layout.RandomLayout;
 
 /**
- * @author Jack Li jack(AT)cs_DOTberkeley_DOTedu
+ * @author Jack Li jack(AT)cs_D0Tberkeley_D0Tedu
  */
 public class TimelineDemo extends JFrame {
+	
 	private static final String MUSIC_HISTORY = "etc/musichistory.xml";
 	private static final String TITLE = "The History of Music";
+	private static final int NUM_DIVISIONS = 0, NOTCH_LENGTH = 1;
+
+	private static final String NOTCH = "notch";
 	
 	private static class TimelineDataRenderer extends TextItemRenderer {
 	}
 
 	public static void main(String[] args) {
+		final int appWidth = 1000;
+		final int appHeight = 400; // this should be more fixed than the width
+		final int divisionSpecification = NOTCH_LENGTH; 
+										  //NUM_DIVISIONS;
+
+		final int timeline_start = 0;
+		final int timeline_end = 2005;
+		
 		// load graph
-		final XMLGraphReader graphReader = new XMLGraphReader();
-		graphReader.setNodeType(DefaultTreeNode.class);
+		//graphReader.setNodeType(DefaultTreeNode.class);
 		final Graph graph;
 		try {
-			graph = graphReader.loadGraph(MUSIC_HISTORY);
+			graph = /*graphReader*/new XMLGraphReader().loadGraph(MUSIC_HISTORY);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return;
 		}
 		
+		// add mark nodes, which may allow dragging, or zooming
+		final int notchLength;
+		final int timeline_length = timeline_end - timeline_start;
+		if (divisionSpecification == NUM_DIVISIONS) {
+			final int numDivisions = 10; // entered desired division in this mode
+			notchLength = timeline_length / numDivisions;
+			for (int i = 0; i < numDivisions; i++) {
+				final int nextNotch = (i * notchLength) + timeline_start;
+				final DefaultNode nextNotchNode = new DefaultNode();
+				nextNotchNode.setAttribute(
+						XMLGraphReader.XMLGraphHandler.ID, NOTCH+i);
+				nextNotchNode.setAttribute(
+						XMLGraphReader.XMLGraphHandler.LABEL, ""+nextNotch);
+				graph.addNode(nextNotchNode);
+			}
+		} else if (divisionSpecification == NOTCH_LENGTH) {
+			notchLength = 311; // entered desired length in this mode
+			for (int nextNotch = timeline_start; nextNotch < timeline_end; nextNotch += notchLength) {
+				final int i = (nextNotch - timeline_start) / notchLength;
+				final DefaultNode nextNotchNode = new DefaultNode();
+				nextNotchNode.setAttribute(
+						XMLGraphReader.XMLGraphHandler.ID, NOTCH+i);
+				nextNotchNode.setAttribute(
+						XMLGraphReader.XMLGraphHandler.LABEL, ""+nextNotch);
+				graph.addNode(nextNotchNode);
+			}
+		}
+
+		
 		final ItemRegistry registry = new ItemRegistry(graph);
 		registry.setRendererFactory(new DefaultRendererFactory(new TimelineDataRenderer(), null, null));
 		
 		final Display display = new Display(registry);
-		display.setSize(1000, 400);
+		display.setSize(appWidth, appHeight);
 		display.addControlListener(new DragControl());
 		
 		// set up this JFrame
