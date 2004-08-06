ad2118e75c41105984d2d0da686b593d0e6b8045
tha_jackl <tha_jackl>
2004-08-06 18:02:34 +0000

    All timeline nodes correctly distorted: renders based on distorted coordinates if exists

--- demos/edu/berkeley/guir/prefuse/timeline/TimelineDataRenderer.java
+++ demos/edu/berkeley/guir/prefuse/timeline/TimelineDataRenderer.java
@@ -41,22 +41,34 @@ public class TimelineDataRenderer extends TextItemRenderer implements TimelineCo
 	 * @see edu.berkeley.guir.prefuse.render.ShapeRenderer#getRawShape(edu.berkeley.guir.prefuse.VisualItem)
 	 */
 	protected Shape getRawShape(VisualItem item) {
-		final String nodeType = item.getAttribute(TimelineDemo.NODE_TYPE);
-		if (nodeType.equals(TimelineDemo.NOTCH_TYPE)) {
-			final String id = item.getAttribute(XMLGraphReader.XMLGraphHandler.ID);
+		final Object leftEndpoint = item.getVizAttribute(LEFT_DISTORTED);
+		if (leftEndpoint == null) { // use normal endpoints
+			return getUndistortedShape(item);
+		} else { // use given endpoints
+			return getDistortedShape(item);
+		}
+	} //
 
-			//getAlignedPoint(m_tmpPoint, item, w, h, m_xAlign, m_yAlign);
+	
+	/**
+	 * @param item
+	 * @return
+	 */
+	private Shape getUndistortedShape(final VisualItem item) {
+		final String nodeType = item.getAttribute(NODE_TYPE);
+		if (nodeType.equals(TimelineDemo.NOTCH_TYPE)) { // notch element
+			final String id = item.getAttribute(XMLGraphReader.XMLGraphHandler.ID);
 			final double itemXCoord = item.getX(), itemYCoord = item.getY();
 			m_line.setLine(itemXCoord, itemYCoord+10, itemXCoord, itemYCoord-10);
-/*			if (id.equals(TimelineDemo.NOTCH_START)) {
+			// in case we want to change appearance of the endpoints
+			/*			if (id.equals(TimelineDemo.NOTCH_START)) {
 			} else if (id.equals(TimelineDemo.NOTCH_END)) {
 			} else { // regular notch
 			}*/
 			return m_line;
 		}
 		else { // data element, for now just have it be a rectangle
-			//get width
-			//one, get start year and end year attributes
+			// 1. get start year and end year attributes
 			final String startYearString = item.getAttribute(START_YEAR);
 			final String endYearString = item.getAttribute(END_YEAR);
 			final int startYear, endYear;
@@ -71,7 +83,7 @@ public class TimelineDataRenderer extends TextItemRenderer implements TimelineCo
 				endYear = new Integer(endYearString).intValue();
 			}
 			
-			//set the nodeLength based on the start & end years
+			// 2. set the nodeLength based on the start & end years
 			final int nodeSpan = endYear - startYear;
 			final int timelineSpan = timelineEnd - timelineStart;
 			nodeLength = timelineLength * nodeSpan / timelineSpan;
@@ -80,9 +92,36 @@ public class TimelineDataRenderer extends TextItemRenderer implements TimelineCo
 			m_textBox.setFrame(m_tmpPoint.getX(),m_tmpPoint.getY(),nodeLength,NODE_HEIGHT);
 			return m_textBox;
 		}
-	} //
-
+	}
 	
+	/**
+	 * @param item
+	 * @return
+	 */
+	private Shape getDistortedShape(final VisualItem item) {
+		final String nodeType = item.getAttribute(NODE_TYPE);
+		if (nodeType.equals(TimelineDemo.NOTCH_TYPE)) { // notch element
+			final String id = item.getAttribute(XMLGraphReader.XMLGraphHandler.ID);
+			final double itemXCoord = item.getX();
+			double itemYCoord = item.getY(); // magnify the y accordingly
+			m_line.setLine(itemXCoord, itemYCoord+10, itemXCoord, itemYCoord-10);
+			return m_line;
+		}
+		else { 
+			final double distortedLeft = ((Double)item.getVizAttribute(
+					LEFT_DISTORTED)).doubleValue();
+			final double distortedRight = ((Double)item.getVizAttribute(
+					RIGHT_DISTORTED)).doubleValue();
+			
+			nodeLength = distortedRight - distortedLeft;//timelineLength * nodeSpan / timelineSpan;
+			//just need to get y_aligned
+			getAlignedPoint(m_tmpPoint, item, nodeLength, NODE_HEIGHT, m_xAlign, m_yAlign);
+			m_textBox.setFrame(distortedLeft,m_tmpPoint.getY(),nodeLength,NODE_HEIGHT);
+			return m_textBox;
+		}
+	}
+
+
 	/**
 	 * @see edu.berkeley.guir.prefuse.render.Renderer#render(java.awt.Graphics2D, edu.berkeley.guir.prefuse.VisualItem)
 	 */
