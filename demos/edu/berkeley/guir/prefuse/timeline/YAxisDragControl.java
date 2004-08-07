dea4596d3285320ce008af86d70992663a31992e
tha_jackl <tha_jackl>
2004-08-07 23:09:43 +0000

    A class to only drag items on the y axis

---
+++ demos/edu/berkeley/guir/prefuse/timeline/YAxisDragControl.java
@@ -0,0 +1,40 @@
+/*
+ * Created on Aug 7, 2004
+ */
+package edu.berkeley.guir.prefuse.timeline;
+
+import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
+
+import javax.swing.SwingUtilities;
+
+import edu.berkeley.guir.prefuse.Display;
+import edu.berkeley.guir.prefuse.NodeItem;
+import edu.berkeley.guir.prefuse.VisualItem;
+import edu.berkeley.guir.prefusex.controls.DragControl;
+
+/**
+ * @author Jack Li jack(AT)cs_D0Tberkeley_D0Tedu
+ */
+public class YAxisDragControl extends DragControl {
+	
+	/* (non-Javadoc)
+	 * @see edu.berkeley.guir.prefuse.event.ControlListener#itemDragged(edu.berkeley.guir.prefuse.VisualItem, java.awt.event.MouseEvent)
+	 */
+	public void itemDragged(final VisualItem item, final MouseEvent e) {
+        if (!(item instanceof NodeItem)) return;
+        if (!SwingUtilities.isLeftMouseButton(e)) return;
+        dragged = true;
+        final Display d = (Display)e.getComponent();
+        tmp = d.getAbsoluteCoordinate(e.getPoint(), tmp);
+        final double dy = tmp.getY()-down.getY();
+        final Point2D p = item.getLocation();
+        item.updateLocation(p.getX(),p.getY()+dy);
+        item.setLocation(p.getX(),p.getY()+dy);
+        down.setLocation(tmp);
+        if ( repaint )
+            item.getItemRegistry().repaint();
+        if ( update != null )
+            update.runNow();
+	}
+}
