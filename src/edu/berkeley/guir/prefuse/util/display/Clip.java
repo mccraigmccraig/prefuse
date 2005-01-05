4a38dcc636ef061b5456a9cd3a225a3260d2cbd3
jheer <jheer>
2005-01-05 01:27:49 +0000

    Made the Clip safe against rotation and shearing.

--- src/edu/berkeley/guir/prefuse/util/display/Clip.java
+++ src/edu/berkeley/guir/prefuse/util/display/Clip.java
@@ -11,12 +11,14 @@ import java.awt.geom.Rectangle2D;
  */
 public class Clip {
     
-    private double[] clip = new double[4];
-    private double[] tmp  = new double[4];
+    private double[] clip = new double[8];
+    private double[] tmp  = new double[8];
     
     public void setClip(double x1, double y1, double x2, double y2) {
         clip[0] = x1; clip[1] = y1;
-        clip[2] = x2; clip[3] = y2;
+        clip[2] = x1; clip[3] = y2;
+        clip[4] = x2; clip[5] = y1;
+        clip[6] = x2; clip[7] = y2;
     } //
     
     public void setClip(Clip c) {
@@ -25,27 +27,48 @@ public class Clip {
     
     public void setClip(Rectangle2D r) {
         clip[0] = r.getX(); clip[1] = r.getY();
-        clip[2] = clip[0]+r.getWidth();
-        clip[3] = clip[1]+r.getHeight();
+        clip[6] = clip[0]+r.getWidth();
+        clip[7] = clip[1]+r.getHeight();
+        clip[2] = clip[0]; clip[3] = clip[7];
+        clip[4] = clip[6]; clip[5] = clip[1];
     } //
     
     public void transform(AffineTransform at) {
-        at.transform(clip,0,tmp,0,2);
+        at.transform(clip,0,tmp,0,4);
         double[] s = tmp;
         tmp = clip;
         clip = s;
+        // make safe against rotation
+        double xmin = clip[0], ymin = clip[1];
+        double xmax = clip[6], ymax = clip[7];
+        for ( int i=0; i<7; i+=2 ) {
+            if ( clip[i] < xmin )
+                xmin = clip[i];
+            if ( clip[i] > xmax )
+                xmax = clip[i];
+            if ( clip[i+1] < ymin )
+                ymin = clip[i+1];
+            if ( clip[i+1] > ymax )
+                ymax = clip[i+1];
+        }
+        clip[0] = xmin; clip[1] = ymin;
+        clip[2] = xmin; clip[3] = ymax;
+        clip[4] = xmax; clip[5] = ymin;
+        clip[6] = xmax; clip[7] = ymax;
     } //
     
     public void limit(double x, double y, double w, double h) {
         clip[0] = Math.max(clip[0],x);
         clip[1] = Math.max(clip[1],y);
-        clip[2] = Math.min(clip[2],w);
-        clip[3] = Math.min(clip[3],h);
+        clip[6] = Math.min(clip[6],w);
+        clip[7] = Math.min(clip[7],h);
+        clip[2] = clip[0]; clip[3] = clip[7];
+        clip[4] = clip[6]; clip[5] = clip[1];
     } //
     
     public boolean intersects(Rectangle2D r) {
-        double tw = clip[2]-clip[0];
-        double th = clip[3]-clip[1];
+        double tw = clip[6]-clip[0];
+        double th = clip[7]-clip[1];
         double rw = r.getWidth();
         double rh = r.getHeight();
         if (rw < 0 || rh < 0 || tw < 0 || th < 0) {
@@ -69,15 +92,15 @@ public class Clip {
     public void union(Clip c) {
         clip[0] = Math.min(clip[0], c.clip[0]);
         clip[1] = Math.min(clip[1], c.clip[1]);
-        clip[2] = Math.max(clip[2], c.clip[2]);
-        clip[3] = Math.max(clip[3], c.clip[3]);
+        clip[2] = Math.max(clip[6], c.clip[6]);
+        clip[3] = Math.max(clip[7], c.clip[7]);
     } //
     
     public void union(Rectangle2D r) {
         clip[0] = Math.min(clip[0], r.getX()-1);
         clip[1] = Math.min(clip[1], r.getY()-1);
-        clip[2] = Math.max(clip[2], r.getX()+r.getWidth()+1);
-        clip[3] = Math.max(clip[3], r.getX()+r.getHeight()+1);
+        clip[2] = Math.max(clip[6], r.getX()+r.getWidth()+1);
+        clip[3] = Math.max(clip[7], r.getX()+r.getHeight()+1);
     } //
     
     public double getX() {
@@ -89,11 +112,11 @@ public class Clip {
     } //
     
     public double getWidth() {
-        return clip[2]-clip[0];
+        return clip[6]-clip[0];
     } //
     
     public double getHeight() {
-        return clip[3]-clip[1];
+        return clip[7]-clip[1];
     } //
     
 } // end of inner class Clip
