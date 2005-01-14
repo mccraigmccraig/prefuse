92e01cc90fa9e238f1b84b61aa2feb82eed5072b
jheer <jheer>
2005-01-14 23:10:56 +0000

    Added getters/setters for color values

--- src/edu/berkeley/guir/prefuse/action/assignment/ColorFunction.java
+++ src/edu/berkeley/guir/prefuse/action/assignment/ColorFunction.java
@@ -19,6 +19,14 @@ import edu.berkeley.guir.prefuse.action.AbstractAction;
  */
 public class ColorFunction extends AbstractAction {
     
+    protected Color defaultColor       = Color.BLACK;
+    protected Color focusColor         = Color.RED;
+    protected Color highlightColor     = Color.BLUE;
+    
+    protected Color defaultFillColor   = Color.WHITE;
+    protected Color focusFillColor     = Color.WHITE;
+    protected Color highlightFillColor = Color.WHITE;
+    
 	public void run(ItemRegistry registry, double frac) {
 		Iterator itemIter = registry.getItems();
 		while ( itemIter.hasNext() ) {
@@ -45,16 +53,106 @@ public class ColorFunction extends AbstractAction {
 
 	public Paint getColor(VisualItem item) {
         if ( item.isFocus() ) {
-            return Color.RED;
+            return focusColor;
         } else if ( item.isHighlighted() ) {
-            return Color.BLUE;
+            return highlightColor;
         } else {
-            return Color.BLACK;   
+            return defaultColor;   
         }
 	} //
 
 	public Paint getFillColor(VisualItem item) {
-        return Color.WHITE;
+	    if ( item.isFocus() ) {
+            return focusFillColor;
+        } else if ( item.isHighlighted() ) {
+            return highlightFillColor;
+        } else {
+            return defaultFillColor;   
+        }
 	} //
 
+    /**
+     * @return Returns the defaultColor.
+     */
+    public Color getDefaultColor() {
+        return defaultColor;
+    } //
+    
+    /**
+     * @param defaultColor The defaultColor to set.
+     */
+    public void setDefaultColor(Color defaultColor) {
+        this.defaultColor = defaultColor;
+    } //
+    
+    /**
+     * @return Returns the defaultFillColor.
+     */
+    public Color getDefaultFillColor() {
+        return defaultFillColor;
+    } //
+    
+    /**
+     * @param defaultFillColor The defaultFillColor to set.
+     */
+    public void setDefaultFillColor(Color defaultFillColor) {
+        this.defaultFillColor = defaultFillColor;
+    } //
+    
+    /**
+     * @return Returns the focusColor.
+     */
+    public Color getFocusColor() {
+        return focusColor;
+    } //
+    
+    /**
+     * @param focusColor The focusColor to set.
+     */
+    public void setFocusColor(Color focusColor) {
+        this.focusColor = focusColor;
+    } //
+    
+    /**
+     * @return Returns the focusFillColor.
+     */
+    public Color getFocusFillColor() {
+        return focusFillColor;
+    } //
+    
+    /**
+     * @param focusFillColor The focusFillColor to set.
+     */
+    public void setFocusFillColor(Color focusFillColor) {
+        this.focusFillColor = focusFillColor;
+    } //
+    
+    /**
+     * @return Returns the highlightColor.
+     */
+    public Color getHighlightColor() {
+        return highlightColor;
+    } //
+    
+    /**
+     * @param highlightColor The highlightColor to set.
+     */
+    public void setHighlightColor(Color highlightColor) {
+        this.highlightColor = highlightColor;
+    } //
+    
+    /**
+     * @return Returns the highlightFillColor.
+     */
+    public Color getHighlightFillColor() {
+        return highlightFillColor;
+    } //
+    
+    /**
+     * @param highlightFillColor The highlightFillColor to set.
+     */
+    public void setHighlightFillColor(Color highlightFillColor) {
+        this.highlightFillColor = highlightFillColor;
+    } //
+    
 } // end of class ColorFunction
