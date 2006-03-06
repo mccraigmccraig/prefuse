ccd4f245c1fe5e9a5e45faa63ee3ff9f26e4326a
jheer <jheer>
2006-03-06 00:31:55 +0000

    Added ExpressionParser unit tests

--- test/test/prefuse/All_Prefuse_Tests.java
+++ test/test/prefuse/All_Prefuse_Tests.java
@@ -10,6 +10,7 @@ public class All_Prefuse_Tests {
         //$JUnit-BEGIN$
         suite.addTest(test.prefuse.data.All_PrefuseData_Tests.suite());
         suite.addTest(test.prefuse.data.column.All_PrefuseDataColumn_Tests.suite());
+        suite.addTest(test.prefuse.data.expression.All_PrefuseDataExpression_Tests.suite());
         suite.addTest(test.prefuse.data.io.All_PrefuseDataIO_Tests.suite());
         suite.addTest(test.prefuse.data.util.All_PrefuseDataUtil_Tests.suite());
         suite.addTest(test.prefuse.visual.All_PrefuseVisual_Tests.suite());
---
+++ test/test/prefuse/data/expression/All_PrefuseDataExpression_Tests.java
@@ -0,0 +1,16 @@
+package test.prefuse.data.expression;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class All_PrefuseDataExpression_Tests {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Test for test.prefuse.data.expression");
+        //$JUnit-BEGIN$
+        suite.addTestSuite(ExpressionTest.class);
+        //$JUnit-END$
+        return suite;
+    }
+
+}
