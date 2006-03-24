2dd6e2477a9bf5a2552798217865b217d2fc9d70
jheer <jheer>
2006-03-24 17:39:37 +0000

    Improved sorting scores

--- src/prefuse/visual/sort/TreeDepthItemSorter.java
+++ src/prefuse/visual/sort/TreeDepthItemSorter.java
@@ -1,7 +1,9 @@
 package prefuse.visual.sort;
 
 import prefuse.Visualization;
+import prefuse.visual.AggregateItem;
 import prefuse.visual.DecoratorItem;
+import prefuse.visual.EdgeItem;
 import prefuse.visual.NodeItem;
 import prefuse.visual.VisualItem;
 
@@ -15,6 +17,12 @@ import prefuse.visual.VisualItem;
  */
 public class TreeDepthItemSorter extends ItemSorter {
 
+    protected static final int AGGREGATE = 0;
+    protected static final int EDGE      = 1;
+    protected static final int ITEM      = 2;
+    protected static final int NODE      = 3;
+    protected static final int DECORATOR = 4;
+    
     private int m_childrenAbove;
     private int m_hover;
     private int m_highlight;
@@ -41,8 +49,8 @@ public class TreeDepthItemSorter extends ItemSorter {
             m_depth = 14;
         } else {
             m_childrenAbove = -1;
-            m_hover = 29;
-            m_highlight = 28;
+            m_hover = 24;
+            m_highlight = 23;
             m_depth = 12;
         }
     }
@@ -53,16 +61,25 @@ public class TreeDepthItemSorter extends ItemSorter {
      * @see prefuse.visual.sort.ItemSorter#score(prefuse.visual.VisualItem)
      */
     public int score(VisualItem item) {
-        int score = 0;
+        int type = ITEM;
+        if ( item instanceof EdgeItem ) {
+            type = EDGE;
+        } else if ( item instanceof AggregateItem ) {
+            type = AGGREGATE;
+        } else if ( item instanceof DecoratorItem ) {
+            type = DECORATOR;
+        }
+        
+        int score = (1<<(25+type));
+        if ( item instanceof NodeItem ) {
+            int depth = ((NodeItem)item).getDepth();
+            score += m_childrenAbove*(depth<<m_depth);
+        }
         if ( item.isHover() ) {
             score += (1<<m_hover);
         }
         if ( item.isHighlighted() ) {
             score += (1<<m_highlight);
-        }
-        if ( item instanceof NodeItem ) {
-            score += (1<<27); // nodes before edges
-            score += m_childrenAbove*(((NodeItem)item).getDepth()<<m_depth);
         }
         if ( item.isInGroup(Visualization.FOCUS_ITEMS) ) {
             score += (1<<11);
@@ -70,10 +87,29 @@ public class TreeDepthItemSorter extends ItemSorter {
         if ( item.isInGroup(Visualization.SEARCH_ITEMS) ) {
             score += (1<<10);
         }
-        if ( item instanceof DecoratorItem ) {
-            score += (1<<9);
-        }
+
         return score;
+//        int score = 0;
+//        if ( item.isHover() ) {
+//            score += (1<<m_hover);
+//        }
+//        if ( item.isHighlighted() ) {
+//            score += (1<<m_highlight);
+//        }
+//        if ( item instanceof NodeItem ) {
+//            score += (1<<27); // nodes before edges
+//            score += m_childrenAbove*(((NodeItem)item).getDepth()<<m_depth);
+//        }
+//        if ( item.isInGroup(Visualization.FOCUS_ITEMS) ) {
+//            score += (1<<11);
+//        }
+//        if ( item.isInGroup(Visualization.SEARCH_ITEMS) ) {
+//            score += (1<<10);
+//        }
+//        if ( item instanceof DecoratorItem ) {
+//            score += (1<<9);
+//        }
+//        return score;
     }
 
 } // end of class TreeDepthItemSorter
