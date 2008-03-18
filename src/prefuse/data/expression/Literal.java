41c5b6e9baa3544e70bc4645c581269786b1cb1f
amarsden <amarsden>
2008-03-18 21:52:16 +0000

    fixed a typo

--- src/prefuse/data/expression/Literal.java
+++ src/prefuse/data/expression/Literal.java
@@ -4,7 +4,7 @@ import prefuse.data.Tuple;
 import prefuse.util.TypeLib;
 
 /**
- * Abstarct base class for a Literal Expression that evaluates to a
+ * Abstract base class for a Literal Expression that evaluates to a
  * constant value.
  *
  * @author <a href="http://jheer.org">jeffrey heer</a>
