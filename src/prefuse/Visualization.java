95fa7e4e1ce51c135e3aa204de2c6a946a875b92
chrisnf <chrisnf>
2008-03-28 08:53:22 +0000

    fixed Javadoc on Visualization.addAggregates(String, Schema)

--- src/prefuse/Visualization.java
+++ src/prefuse/Visualization.java
@@ -540,7 +540,7 @@ public class Visualization {
      * Add a group of aggregates to this visualization. Aggregates are
      * used to visually represent groups of VisualItems.  
      * @param group the data group name for the aggregates. 
-     * @param schema the data schema to use for the AggregateTable, should usually extend VisualItem.SCHEMA
+     * @param schema the data schema to use for the AggregateTable, should usually extend PrefuseLib.getVisualItemSchema()
      * @return the generated AggregateTable
      * @see prefuse.visual.AggregateTable
      */
