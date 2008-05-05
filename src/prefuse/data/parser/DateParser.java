b1a6238bf265dcb2440ea2c284af0230c2b6256b
jogiles <jogiles>
2008-05-05 00:55:36 +0000

    Improved the JavaDateParser to handle more date formats.

--- src/prefuse/data/parser/DateParser.java
+++ src/prefuse/data/parser/DateParser.java
@@ -6,81 +6,92 @@ import java.text.ParsePosition;
 import java.util.Locale;
 
 /**
- * DataParser instance that parses Date values as java.sql.Time instances,
- * representing a particular date (but not a specific time on that day).
- * This class uses a backing {@link java.text.DateFormat} instance to
- * perform parsing. The DateFormat instance to use can be passed in to the
- * constructor, or by default the DateFormat returned by
- * {@link java.text.DateFormat#getDateInstance(int)} with an
- * argument of {@link java.text.DateFormat#SHORT} is used.
- *
+ * DataParser instance that parses Date values as java.sql.Time instances, representing a particular
+ * date (but not a specific time on that day). This class uses a backing
+ * {@link java.text.DateFormat} instance to perform parsing. The DateFormat instance to use can be
+ * passed in to the constructor, or by default the DateFormat returned by
+ * {@link java.text.DateFormat#getDateInstance(int)} with an argument of
+ * {@link java.text.DateFormat#SHORT} is used.
+ * 
  * @author <a href="http://jheer.org">jeffrey heer</a>
  */
 public class DateParser extends AbstractDateParser {
 
-    protected DateFormat    m_dfmt;
-    protected ParsePosition m_pos;
+	protected DateFormat m_dfmt;
+	protected ParsePosition m_pos;
 
-    /**
-     * Create a new DateParser.
-     */
-    public DateParser() {
-        this(DateFormat.getDateInstance(DateFormat.SHORT));
-    }
-
-    /**
-     * Create a new DateParser.
-     * @param dateFormat the DateFormat instance to use for parsing
-     */
-    public DateParser(DateFormat dateFormat) {
-        m_dfmt = dateFormat;
-        m_pos = new ParsePosition(0);
-    }
-
-    public DateParser(Locale locale) {
-    	this(DateFormat.getDateInstance(DateFormat.SHORT, locale));
+	/**
+	 * Create a new DateParser.
+	 */
+	public DateParser() {
+		this(DateFormat.getDateInstance(DateFormat.SHORT));
 	}
 
 	/**
-     * Returns java.sql.Date.
-     * @see prefuse.data.parser.DataParser#getType()
-     */
-    public Class<?> getType() {
-        return Date.class;
-    }
+	 * Create a new DateParser.
+	 * 
+	 * @param dateFormat
+	 *            the DateFormat instance to use for parsing
+	 */
+	public DateParser(final DateFormat dateFormat) {
+		m_dfmt = dateFormat;
+		m_pos = new ParsePosition(0);
+	}
 
-    /**
-     * Parse a Date value from a text string.
-     * @param text the text string to parse
-     * @return the parsed Date value
-     * @throws DataParseException if an error occurs during parsing
-     */
-    public java.util.Date parseDate(String text) throws DataParseException {
-        m_pos.setErrorIndex(0);
-        m_pos.setIndex(0);
+	public DateParser(final Locale locale) {
+		this(DateFormat.getDateInstance(DateFormat.SHORT, locale));
+	}
 
-        // parse the data value, convert to the wrapper type
-        Date d = null;
-        try {
-            d = Date.valueOf(text);
-            m_pos.setIndex(text.length());
-        } catch ( IllegalArgumentException e ) {
-            d = null;
-        }
-        if ( d == null ) {
-            java.util.Date d1 = m_dfmt.parse(text, m_pos);
-            if ( d1 != null ) {
-                d = new Date(d1.getTime());
-            }
-        }
+	/**
+	 * Returns java.sql.Date.
+	 * 
+	 * @see prefuse.data.parser.DataParser#getType()
+	 */
+	@Override
+	public Class<?> getType() {
+		return Date.class;
+	}
 
-        // date format will parse substrings successfully, so we need
-        // to check the position to make sure the whole value was used
-        if ( d == null || m_pos.getIndex() < text.length() ) {
-            throw new DataParseException("Could not parse Date: "+text);
-        } else {
-            return d;
-        }
-    }
+	/**
+	 * Parse a Date value from a text string.
+	 * 
+	 * @param text
+	 *            the text string to parse
+	 * @return the parsed Date value
+	 * @throws DataParseException
+	 *             if an error occurs during parsing
+	 */
+	@Override
+	public java.util.Date parseDate(final String text) throws DataParseException {
+		m_pos.setErrorIndex(0);
+		m_pos.setIndex(0);
+
+		// parse the data value, convert to the wrapper type
+		Date d = null;
+
+		if (d == null) {
+			try {
+				d = Date.valueOf(text);
+				m_pos.setIndex(text.length());
+			} catch (final IllegalArgumentException e) {
+				d = null;
+			}
+		}
+
+		if (d == null) {
+			final java.util.Date d1 = m_dfmt.parse(text, m_pos);
+			if (d1 != null) {
+				d = new Date(d1.getTime());
+			}
+		}
+
+		// date format will parse substrings successfully, so we need
+		// to check the position to make sure the whole value was used
+		if (d == null || m_pos.getIndex() < text.length()) {
+			throw new DataParseException("Could not parse Date: " + text);
+		} else {
+			return d;
+		}
+	}
 
 } // end of class DateParser
--- src/prefuse/data/parser/JavaDateParser.java
+++ src/prefuse/data/parser/JavaDateParser.java
@@ -5,37 +5,35 @@ import java.util.Date;
 import java.util.Locale;
 
 /**
- * DataParser instance that parses Date values as java.util.Date instances,
- * representing a particular date and time.
- * This class uses a backing {@link java.text.DateFormat} instance to
- * perform parsing. The DateFormat instance to use can be passed in to the
- * constructor, or by default the DateFormat returned by
- * {@link java.text.DateFormat#getDateTimeInstance(int, int)} with both
- * arguments being {@link java.text.DateFormat#SHORT} is used.
- *
+ * DataParser instance that parses Date values as java.util.Date instances, representing a
+ * particular date and time. This class uses a backing {@link java.text.DateFormat} instance to
+ * perform parsing. The DateFormat instance to use can be passed in to the constructor, or by
+ * default the DateFormat returned by {@link java.text.DateFormat#getDateTimeInstance(int, int)}
+ * with both arguments being {@link java.text.DateFormat#SHORT} is used.
+ * 
  * @author <a href="http://jheer.org">jeffrey heer</a>
  */
 public class JavaDateParser extends AbstractDateParser {
 
-    /**
-     * Create a new DateTimeParser.
-     */
-    public JavaDateParser() {
-        this(DateFormat.getDateTimeInstance(
-                DateFormat.SHORT, DateFormat.SHORT));
-    }
+	/**
+	 * Create a new DateTimeParser.
+	 */
+	public JavaDateParser() {
+		this(DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT));
+	}
 
-    /**
-     * Create a new DateTimeParser.
-     * @param dateFormat the DateFormat instance to use for parsing
-     */
-    public JavaDateParser(DateFormat dateFormat) {
-        super(dateFormat);
-    }
+	/**
+	 * Create a new DateTimeParser.
+	 * 
+	 * @param dateFormat
+	 *            the DateFormat instance to use for parsing
+	 */
+	public JavaDateParser(final DateFormat dateFormat) {
+		super(dateFormat);
+	}
 
-	public JavaDateParser(Locale locale) {
-        this(DateFormat.getDateTimeInstance(
-                DateFormat.SHORT, DateFormat.SHORT, locale));
+	public JavaDateParser(final Locale locale) {
+		this(DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, locale));
 	}
 
 	@Override
@@ -44,27 +42,35 @@ public class JavaDateParser extends AbstractDateParser {
 	}
 
 	@Override
-	public Date parseDate(String text) throws DataParseException {
-        m_pos.setErrorIndex(0);
-        m_pos.setIndex(0);
+	public Date parseDate(final String text) throws DataParseException {
+		m_pos.setErrorIndex(0);
+		m_pos.setIndex(0);
 
-        // parse the data value, convert to the wrapper type
-        Date d = null;
-        try {
-            d = new Date(java.sql.Date.valueOf(text).getTime());
-            m_pos.setIndex(text.length());
-        } catch ( IllegalArgumentException e ) {
-            d = m_dfmt.parse(text, m_pos);
-        }
+		// parse the data value, convert to the wrapper type
+		Date d = null;
+		try {
+			d = new Date(java.sql.Date.valueOf(text).getTime());
+			m_pos.setIndex(text.length());
+		} catch (final IllegalArgumentException e) {
+			d = m_dfmt.parse(text, m_pos);
+		}
 
-        // date format will parse substrings successfully, so we need
-        // to check the position to make sure the whole value was used
-        if ( d == null || m_pos.getIndex() < text.length() ) {
-            throw new DataParseException("Could not parse Date: "+text);
-        } else {
-            return d;
-        }
+		if (d == null) {
+			try {
+				d = CalendarParser.parse(text).getTime();
+				m_pos.setIndex(text.length());
+			} catch (final CalendarParserException e) {
+				d = null;
+			}
+		}
+
+		// date format will parse substrings successfully, so we need
+		// to check the position to make sure the whole value was used
+		if (d == null || m_pos.getIndex() < text.length()) {
+			throw new DataParseException("Could not parse Date: " + text);
+		} else {
+			return d;
+		}
 	}
 
 }
-
