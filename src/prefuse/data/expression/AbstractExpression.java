6899f793b0242623bd7f30fbb395003bc5dce266
jogiles <jogiles>
2008-06-03 22:35:22 +0000

    applied memory leak patch provided by 'code_m0nkey' here: http://sourceforge.net/tracker/index.php?func=detail&aid=1969196&group_id=98962&atid=622647

--- src/prefuse/data/expression/AbstractExpression.java
+++ src/prefuse/data/expression/AbstractExpression.java
@@ -6,132 +6,137 @@ import prefuse.data.Tuple;
 import prefuse.data.event.ExpressionListener;
 
 /**
- * Abstract base class for Expression implementations. Provides support for
- * listeners and defaults every Expression evaluation method to an
- * unsupported operation.
- *
+ * Abstract base class for Expression implementations. Provides support for listeners and defaults
+ * every Expression evaluation method to an unsupported operation.
+ * 
  * @author <a href="http://jheer.org">jeffrey heer</a>
  */
-public abstract class AbstractExpression
-    implements Expression, ExpressionListener
-{
-    private final CopyOnWriteArrayList<ExpressionListener> m_listeners = new CopyOnWriteArrayList<ExpressionListener>();
+public abstract class AbstractExpression implements Expression, ExpressionListener {
+	private final CopyOnWriteArrayList<ExpressionListener> m_listeners = new CopyOnWriteArrayList<ExpressionListener>();
 
-    /**
-     * @see prefuse.data.expression.Expression#visit(prefuse.data.expression.ExpressionVisitor)
-     */
-    public void visit(ExpressionVisitor v) {
-        v.visitExpression(this);
-    }
+	/**
+	 * @see prefuse.data.expression.Expression#visit(prefuse.data.expression.ExpressionVisitor)
+	 */
+	public void visit(final ExpressionVisitor v) {
+		v.visitExpression(this);
+	}
 
-    /**
-     * @see prefuse.data.expression.Expression#addExpressionListener(prefuse.data.event.ExpressionListener)
-     */
-    public final void addExpressionListener(ExpressionListener lstnr) {
-        if ( !m_listeners.contains(lstnr) ) {
-            m_listeners.add(lstnr);
-            addChildListeners();
-        }
-    }
+	/**
+	 * @see prefuse.data.expression.Expression#addExpressionListener(prefuse.data.event.ExpressionListener)
+	 */
+	public void addExpressionListener(final ExpressionListener lstnr) {
+		if (!m_listeners.contains(lstnr)) {
+			m_listeners.add(lstnr);
+			addChildListeners();
+		}
+	}
 
-    /**
-     * @see prefuse.data.expression.Expression#removeExpressionListener(prefuse.data.event.ExpressionListener)
-     */
-    public final void removeExpressionListener(ExpressionListener lstnr) {
-        m_listeners.remove(lstnr);
-        if ( m_listeners.size() == 0 ) {
+	/**
+	 * @see prefuse.data.expression.Expression#removeExpressionListener(prefuse.data.event.ExpressionListener)
+	 */
+	public void removeExpressionListener(final ExpressionListener lstnr) {
+		m_listeners.remove(lstnr);
+		if (m_listeners.size() == 0) {
 			removeChildListeners();
 		}
-    }
+	}
 
-    /**
-     * Indicates if any listeners are registered with this Expression.
-     * @return true if listeners are registered, false otherwise
-     */
-    protected final boolean hasListeners() {
-        return m_listeners != null && m_listeners.size() > 0;
-    }
+	/**
+	 * Indicates if any listeners are registered with this Expression.
+	 * 
+	 * @return true if listeners are registered, false otherwise
+	 */
+	protected final boolean hasListeners() {
+		return m_listeners != null && m_listeners.size() > 0;
+	}
 
-    /**
-     * Fire an expression change.
-     */
-    protected final void fireExpressionChange() {
-        for(ExpressionListener l : m_listeners) {
-        	l.expressionChanged(this);
-        }
-    }
+	/**
+	 * Fire an expression change.
+	 */
+	protected final void fireExpressionChange() {
+		for (final ExpressionListener l : m_listeners) {
+			l.expressionChanged(this);
+		}
+	}
 
-    /**
-     * Add child listeners to catch and propagate sub-expression updates.
-     */
-    protected void addChildListeners() {
-        // nothing to do
-    }
+	/**
+	 * Add child listeners to catch and propagate sub-expression updates.
+	 */
+	protected void addChildListeners() {
+	// nothing to do
+	}
 
-    /**
-     * Remove child listeners for sub-expression updates.
-     */
-    protected void removeChildListeners() {
-        // nothing to do
-    }
+	/**
+	 * Remove child listeners for sub-expression updates.
+	 */
+	protected void removeChildListeners() {
+	// nothing to do
+	}
 
-    /**
-     * Relay an expression change event.
-     * @see prefuse.data.event.ExpressionListener#expressionChanged(prefuse.data.expression.Expression)
-     */
-    public void expressionChanged(Expression expr) {
-        fireExpressionChange();
-    }
+	/**
+	 * Relay an expression change event.
+	 * 
+	 * @see prefuse.data.event.ExpressionListener#expressionChanged(prefuse.data.expression.Expression)
+	 */
+	public void expressionChanged(final Expression expr) {
+		fireExpressionChange();
+	}
 
-    // ------------------------------------------------------------------------
-    // Default Implementation
+	// ------------------------------------------------------------------------
+	// Default Implementation
 
-    /**
-     * By default, throws an UnsupportedOperationException.
-     * @see prefuse.data.expression.Expression#get(prefuse.data.Tuple)
-     */
-    public Object get(Tuple<?> t) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * By default, throws an UnsupportedOperationException.
+	 * 
+	 * @see prefuse.data.expression.Expression#get(prefuse.data.Tuple)
+	 */
+	public Object get(final Tuple<?> t) {
+		throw new UnsupportedOperationException();
+	}
 
-    /**
-     * By default, throws an UnsupportedOperationException.
-     * @see prefuse.data.expression.Expression#getInt(prefuse.data.Tuple)
-     */
-    public int getInt(Tuple<?> t) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * By default, throws an UnsupportedOperationException.
+	 * 
+	 * @see prefuse.data.expression.Expression#getInt(prefuse.data.Tuple)
+	 */
+	public int getInt(final Tuple<?> t) {
+		throw new UnsupportedOperationException();
+	}
 
-    /**
-     * By default, throws an UnsupportedOperationException.
-     * @see prefuse.data.expression.Expression#getLong(prefuse.data.Tuple)
-     */
-    public long getLong(Tuple<?> t) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * By default, throws an UnsupportedOperationException.
+	 * 
+	 * @see prefuse.data.expression.Expression#getLong(prefuse.data.Tuple)
+	 */
+	public long getLong(final Tuple<?> t) {
+		throw new UnsupportedOperationException();
+	}
 
-    /**
-     * By default, throws an UnsupportedOperationException.
-     * @see prefuse.data.expression.Expression#getFloat(prefuse.data.Tuple)
-     */
-    public float getFloat(Tuple<?> t) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * By default, throws an UnsupportedOperationException.
+	 * 
+	 * @see prefuse.data.expression.Expression#getFloat(prefuse.data.Tuple)
+	 */
+	public float getFloat(final Tuple<?> t) {
+		throw new UnsupportedOperationException();
+	}
 
-    /**
-     * By default, throws an UnsupportedOperationException.
-     * @see prefuse.data.expression.Expression#getDouble(prefuse.data.Tuple)
-     */
-    public double getDouble(Tuple<?> t) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * By default, throws an UnsupportedOperationException.
+	 * 
+	 * @see prefuse.data.expression.Expression#getDouble(prefuse.data.Tuple)
+	 */
+	public double getDouble(final Tuple<?> t) {
+		throw new UnsupportedOperationException();
+	}
 
-    /**
-     * By default, throws an UnsupportedOperationException.
-     * @see prefuse.data.expression.Expression#getBoolean(prefuse.data.Tuple)
-     */
-    public boolean getBoolean(Tuple<?> t) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * By default, throws an UnsupportedOperationException.
+	 * 
+	 * @see prefuse.data.expression.Expression#getBoolean(prefuse.data.Tuple)
+	 */
+	public boolean getBoolean(final Tuple<?> t) {
+		throw new UnsupportedOperationException();
+	}
 
 } // end of abstract class AbstractExpression
--- src/prefuse/visual/expression/VisiblePredicate.java
+++ src/prefuse/visual/expression/VisiblePredicate.java
@@ -9,52 +9,63 @@ import prefuse.visual.VisualItem;
 
 /**
  * Expression that indicates if an item's visible flag is set.
- *
+ * 
  * @author <a href="http://jheer.org">jeffrey heer</a>
  */
-public class VisiblePredicate extends ColumnExpression
-    implements Predicate, Function
-{
+public class VisiblePredicate extends ColumnExpression implements Predicate, Function {
 
-    /** Convenience instance for the visible == true case. */
-    public static final Predicate TRUE = new VisiblePredicate();
-    /** Convenience instance for the visible == false case. */
-    public static final Predicate FALSE = new NotPredicate(TRUE);
+	/** Convenience instance for the visible == true case. */
+	public static final Predicate TRUE = new VisiblePredicate() {
+		@Override
+		public void addExpressionListener(prefuse.data.event.ExpressionListener lstnr) {
+		// do nothing; this instance is a constant, so it will never need
+		// to notify an ExpressionListener
+		}
+	};
 
-    /**
-     * Create a new VisiblePredicate.
-     */
-    public VisiblePredicate() {
-        super(VisualItem.VISIBLE);
-    }
+	/** Convenience instance for the visible == false case. */
+	public static final Predicate FALSE = new NotPredicate(TRUE) {
+		@Override
+		public void addExpressionListener(prefuse.data.event.ExpressionListener lstnr) {
+		// do nothing; this instance is a constant, so it will never need
+		// to notify an ExpressionListener
+		}
+	};
 
-    /**
-     * @see prefuse.data.expression.Function#getName()
-     */
-    public String getName() {
-        return "VISIBLE";
-    }
+	/**
+	 * Create a new VisiblePredicate.
+	 */
+	public VisiblePredicate() {
+		super(VisualItem.VISIBLE);
+	}
 
-    /**
-     * @see prefuse.data.expression.Function#addParameter(prefuse.data.expression.Expression)
-     */
-    public void addParameter(Expression e) {
-        throw new IllegalStateException("This function takes 0 parameters");
-    }
+	/**
+	 * @see prefuse.data.expression.Function#getName()
+	 */
+	public String getName() {
+		return "VISIBLE";
+	}
 
-    /**
-     * @see prefuse.data.expression.Function#getParameterCount()
-     */
-    public int getParameterCount() {
-        return 0;
-    }
+	/**
+	 * @see prefuse.data.expression.Function#addParameter(prefuse.data.expression.Expression)
+	 */
+	public void addParameter(final Expression e) {
+		throw new IllegalStateException("This function takes 0 parameters");
+	}
 
-    /**
-     * @see java.lang.Object#toString()
-     */
-    @Override
+	/**
+	 * @see prefuse.data.expression.Function#getParameterCount()
+	 */
+	public int getParameterCount() {
+		return 0;
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
 	public String toString() {
-        return getName()+"()";
-    }
+		return getName() + "()";
+	}
 
 } // end of class VisiblePredicate
